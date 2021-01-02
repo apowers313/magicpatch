9bd74a4d6c99f32e222bd8bcf06a8878770036e5
Adam Powers <apowers@ato.ms>
2021-01-01 21:53:09 -0800

    add dhist

---
+++ test/testMagicDhist.js
@@ -0,0 +1,37 @@
+/* global $$ */
+
+require("./helpers/magicpatch");
+const {runCode, testMagic} = require("./helpers/helpers");
+const {assert} = require("chai");
+const path = require("path");
+
+let startDir = process.cwd();
+
+describe.only("%dhist", function() {
+    afterEach(function() {
+        process.chdir(startDir);
+        $$.addMagic.utils.setHiddenGlobal("_dh", [process.cwd()]);
+    });
+
+    it("shows history", async function() {
+        let destPath = path.resolve(startDir, "..");
+
+        await testMagic(
+            // magic command
+            "%cd -q ..\n" +
+            "%dhist",
+            // return value
+            undefined,
+            // stdout
+            [
+                "Directory history (kept in _dh",
+                `0: ${startDir}`,
+                `1: ${destPath}`,
+            ],
+            // stderr
+            [],
+            // print output
+            // true,
+        );
+    });
+});
